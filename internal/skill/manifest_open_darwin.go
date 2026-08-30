faa5b5d5cd80b4783a8e57a85b3a9ae9e88e4dd3
Javier Campanini <jmcampanini@gmail.com>
2026-08-29 21:37:07 -0400

    refactor: simplify manifest opening

--- internal/skill/manifest_open_darwin.go
+++ internal/skill/manifest_open_darwin.go
@@ -2,30 +2,4 @@
 
 package skill
 
-import (
-	"os"
-	"syscall"
-	"unsafe"
-)
-
 const sysOpenat = 463
-
-func openManifestAt(directory *os.File) (*os.File, error) {
-	name, err := syscall.BytePtrFromString(manifestName)
-	if err != nil {
-		return nil, err
-	}
-	fd, _, errno := syscall.Syscall6(
-		sysOpenat,
-		directory.Fd(),
-		uintptr(unsafe.Pointer(name)),
-		uintptr(os.O_RDONLY|syscall.O_NOFOLLOW|syscall.O_NONBLOCK|syscall.O_CLOEXEC),
-		0,
-		0,
-		0,
-	)
-	if errno != 0 {
-		return nil, errno
-	}
-	return os.NewFile(fd, manifestName), nil
-}
